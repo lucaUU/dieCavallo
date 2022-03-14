5ae8fbc38bb82d92800d517ba8cd486e7b629ac7
Luca Mueller <luca.mueller@isolutions.ch>
2022-03-14 12:51:26 +0100

    intial version of the best name generator for your baby

---
+++ NameGenerator/Generator/Geni.cs
@@ -0,0 +1,21 @@
+﻿namespace Generator
+{
+    public class Geni
+    {
+        public string GenerateName(int size)
+        {
+            var availableChars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            string name = string.Empty;
+
+            for(int i = 0; i < size; i++)
+            {
+                var random = new System.Random();
+                var get = random.Next(0, maxValue: availableChars.Length - 1);
+
+                name = name + availableChars[get];
+            }
+
+            return name;
+        }
+    }
+}
